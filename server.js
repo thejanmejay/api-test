e6cd6670091e6813a4d4e134c5e613244943461c
Janmejay Singh <janmejaysingh331@gmail.com>
2025-05-03 14:28:20 +0530

    front end integration

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');  // Import the app from app.js
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
